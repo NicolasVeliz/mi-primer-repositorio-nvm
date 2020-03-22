2197441462cb2fe9d073845f6cfa42777c4e9aae
Nicolas Veliz <niveliz@miuandes.cl>
2020-03-22 00:06:30 -0300

    first commit

---
+++ mi-primer-repositorio-nvm/mi-primer-repositorio-nvm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mi_primer_repositorio_nvm
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+        }
+    }
+}
